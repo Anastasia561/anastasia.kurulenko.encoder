1d0a6189345ffa1a6e23da9ceb3611773f920d6c
Anastasiia Kurulenko <kurulenkoanastasia@gmail.com>
2023-08-27 19:37:54 +0300

    FileService implemented

---
+++ src/main/java/ua/javarush/encoder/Runner.java
@@ -0,0 +1,14 @@
+package ua.javarush.encoder;
+
+public class Runner {
+    public  static int key;
+    public static String commandToAdd;
+    public  static String startFileName;
+    public  static String command;
+    public String[] args;
+
+    public Runner(String[] args) {
+        this.args = args;
+    }
+
+}
